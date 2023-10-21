de7bef4ec7fbea71cc418777a64799a82640af89
mspark109 <minsup109@gmail.com>
2023-10-21 18:00:00 +0900

    이미지 로딩실패시 영화 숨김처리 추가

--- src/js/components/Movie.js
+++ src/js/components/Movie.js
@@ -1,13 +1,26 @@
 import { PropTypes } from "prop-types";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import styles from "../../css/components/Movie.module.css";
 
 function Movie({ id, coverImg, title, rating, genres }) {
+  const [error, setError] = useState(false);
+
+  /* 이미지 로딩 실패시 요소 hidden 처리 */
+  const handleImageError = () => {
+    setError(true);
+  };
+
   return (
-    <div className={styles.movie}>
+    <div className={styles.movie} style={{ display: error ? "none" : "block" }}>
       <div className={styles.movie_img_box}>
         <Link to={`/movie/${id}`}>
-          <img src={coverImg} alt={title} className={styles.movie_img} />
+          <img
+            src={coverImg}
+            alt={title}
+            className={styles.movie_img}
+            onError={handleImageError}
+          />
           <span className={styles.movie_rating}>{`★${rating}/10`}</span>
         </Link>
         <h2 className={styles.movie_title}>{title}</h2>
